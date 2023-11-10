f4e04257d01c22c1aeaf4ea272e6fc54eebeac4b
oseji <jiade1233@gmail.com>
2023-11-10 14:28:48 +0100

    added animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "7xl": "7.5rem",
+      },
+    },
   },
   plugins: [],
 };
